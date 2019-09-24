285a59f15cdf4e297a549f2dc961e28c0a7ce8a7
scoombe <skatesam95@gmail.com>
2019-09-24 16:52:43 +0100

    Finsished migration to typescript for scores page, needs testing though

--- src/Pages/ScoresPage/index.tsx
+++ src/Pages/ScoresPage/index.tsx
@@ -1,34 +1,109 @@
 import * as React from 'react';
 import {
-    Grid, Icon, List,
+    Grid, Header, Icon, List,
 } from 'semantic-ui-react';
-import Header from '../../Components/Header';
+import TypingHeader from '../../Components/TypingHeader';
+import { IScoreObj } from '../../Core/definitions';
 import { getUserScores } from '../../Core/firebase-functions';
+import { sortObj } from '../../Core/sort-functions';
 
 interface IState {
-  loggedIn: boolean;
+  scores: {
+    [key: string]: IScoreObj;
+  };
 }
 
 class ScoresPage extends React.Component<{}, IState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      loggedIn: false,
+      scores: {},
     };
     this.loggedIn = this.loggedIn.bind(this);
+    this.getUserScoresCallback = this.getUserScoresCallback.bind(this);
+    this.userScoreElements = this.userScoreElements.bind(this);
+    this.sortedUserScoreElements = this.sortedUserScoreElements.bind(this);
+    this.returnScoreListItem = this.returnScoreListItem.bind(this);
   }
   public render() {
     return(
       <Grid>
         <Grid.Column width={16} >
-          <Header page="scores" loggedIn={this.loggedIn}/>
+          <TypingHeader page="scores" loggedIn={this.loggedIn}/>
         </Grid.Column>
+        <Grid.Row centered={true}>
+          <Grid.Column width="3"/>
+          <Grid.Column width="4">
+            <Header as="h2" icon={true} textAlign="center">
+              <Icon name="user" circular={true} />
+              <Header.Content>User Scores</Header.Content>
+            </Header>
+            <List size="large" celled={true}>
+              {
+                this.userScoreElements()
+              }
+            </List>
+          </Grid.Column>
+          <Grid.Column width="4">
+            <Header as="h2" icon={true} textAlign="center">
+              <Icon name="user" circular={true} />
+              <Header.Content>Best User Scores</Header.Content>
+            </Header>
+            <List size="large" celled={true}>
+              {
+                this.sortedUserScoreElements()
+              }
+            </List>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
 
   private loggedIn() {
-    this.setState({ loggedIn: true });
+    getUserScores(this.getUserScoresCallback);
+  }
+
+  private getUserScoresCallback(score: IScoreObj) {
+    const { scores } = this.state;
+    scores[score.key] = score;
+    this.setState({ scores });
+  }
+
+  private userScoreElements(): JSX.Element[] {
+    const { scores } = this.state;
+    return Object.keys(scores).map((key: string) => {
+      return this.returnScoreListItem(scores[key], false);
+    });
+  }
+
+  private sortedUserScoreElements(): JSX.Element[] {
+    const { scores } = this.state;
+    const sortedScores: IScoreObj[] =  Object.keys(scores).map((key: string) => {
+      return scores[key];
+    });
+    sortedScores.sort(sortObj('-WPM'));
+    const sortedScoreElements: JSX.Element[] = [];
+    let firstScore = true;
+    for (const score of sortedScores) {
+      sortedScoreElements.push(
+        this.returnScoreListItem(score, firstScore),
+      );
+      firstScore = false;
+    }
+    return sortedScoreElements;
+  }
+
+  private returnScoreListItem(score: IScoreObj, first: boolean): JSX.Element {
+    return(
+      <List.Item key={score.key}>
+      <List.Content>
+        <List.Header>{score.userName}  {first && <Icon name="trophy" color="yellow" />}</List.Header>
+        WPM: {score.WPM}  <br />
+        Average WPM: {score.averageWPM}
+      </List.Content>
+    </List.Item>
+    );
   }
 }
 
